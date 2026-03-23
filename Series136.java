43869c460df8650e586917ecac314053e3bdea2e
Anshika <anshikagujral78@gmail.com>
2026-03-23 17:45:18 +0530

    Add files via upload

---
+++ Series136.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Series136 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 0)
+                System.out.print("-" + i + " ");
+            else
+                System.out.print(i + " ");
+        }
+    }
+}
---
+++ SumOfFractionalSeries.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumOfFractionalSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        double sum = 0;
+        for(int i = 1; i <= n; i++) {
+            sum += (double)i / (i + 1);
+        }
+
+        System.out.println(sum);
+    }
+}
